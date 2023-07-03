f24dd3b44702122f995c0feb1dfcc7475539bd4e
aniela <aniela.fendrych@gmail.com>
2023-07-03 18:51:13 +0200

    28-js-warunki

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ let array = [1, 2, 3, 4];
 let object = document.selector(h1);
 */
 
-alert("Skrypt działa prawidłowo!");
+let number = 12;
+ if (number > 10){
+    console.log('Podana liczba jest większa od 10');
+ }
+ else {
+    console.log('Podana liczba nie jest większa od 10');
+ }
