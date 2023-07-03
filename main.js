1cb8db2fdd07a9bed6a1772fea8646456f93ccbc
aniela <aniela.fendrych@gmail.com>
2023-07-03 18:46:03 +0200

    27-js-podpinanie-skryptu

--- main.js
+++ main.js
@@ -4,8 +4,4 @@ let array = [1, 2, 3, 4];
 let object = document.selector(h1);
 */
 
-let col1 = 'white';
-let col2 = 'red';
-let col3 = 'blue';
-
-let array = [col1, col2, col3];
+alert("Skrypt działa prawidłowo!");
