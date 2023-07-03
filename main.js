e4cf4d9ec538bcfa8eb7551ec3f0653827d9f097
aniela <aniela.fendrych@gmail.com>
2023-07-03 19:22:05 +0200

    29-js-suma-zmiennych

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-/* console.log('Nela!'); 
-let name = 'Nela';
-let array = [1, 2, 3, 4];
-let object = document.selector(h1);
-*/
+console.log('Nela!'); 
+let firstNumber = 1;
+let secondNumber = 3;
 
-let number = 12;
- if (number > 10){
-    console.log('Podana liczba jest większa od 10');
- }
- else {
-    console.log('Podana liczba nie jest większa od 10');
- }
+function suma(a, b) {
+    return a + b;
+}
+console.log ( suma(firstNumber, secondNumber));
