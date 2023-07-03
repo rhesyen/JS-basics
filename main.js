950942cc45057433110eb1061e5da4a9d0910a3a
aniela <aniela.fendrych@gmail.com>
2023-07-03 19:56:46 +0200

    31-js-zdarzenie-onclick

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let paragraph = document.querySelector('p');
+/* let paragraph = document.querySelector('p');
 console.log(paragraph);
 paragraph.textContent = 'Teraz to!';
-console.log(paragraph);
+console.log(paragraph); */
+
+let button = document.querySelector('button');
+button.onclick = function() {
+    alert('Klik działa!');
+}
