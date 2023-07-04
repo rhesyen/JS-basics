6b1755073d11ce065a453f8f5997d400853cedbe
aniela <aniela.fendrych@gmail.com>
2023-07-04 19:49:58 +0200

    7-js-podpinanie-skryptu-poprawione

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-/* let paragraph = document.querySelector('p');
-console.log(paragraph);
-paragraph.textContent = 'Teraz to!';
-console.log(paragraph); */
-
-let button = document.querySelector('button');
-button.onclick = function() {
-    alert('Klik działa!');
-}
+alert("Skrypt działa prawidłowo!");
